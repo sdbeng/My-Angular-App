05c29a995d1f86d003d75426205d8edf6f7f10ef
Sergio Barrera <sdbeng@gmail.com>
2014-09-02 16:12:17 -0700

    Add firebase service to wailist controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,13 +7,39 @@ angular.module('myApp.controllers', [])
  .controller('LandingPageController', ['', function(){
  	
  }])
- .controller('WaitlistController', ['', function(){
+ .controller('WaitlistController', ['$scope', '$firebase', function($scope,$firebase){
+ 	//var parties Reference here
+ 	var partiesRef = new Firebase('https://appointments-sdb.firebaseio.com/');
+ 	//Then, we can use the firebase service we've just injected to create a 
+ 	//firebase object that holds off the data at this url
+ 	//so we will replace the scope.parties witht the firebase live data
+ 	//it takes the partiesRef
+ 	$scope.parties = $firebase(partiesRef);
+
+ 	// $scope.testVar = 'Serg'
  	
+ 	//In here this scope will list all the diff parties added with the form
+ 	// $scope.parties = [];
+ 	//Now, will replace the array above with live data from firebase
+ 	//put the variable reference first as the first line though!
+
+ 	//Next, we want to have an object in our scope, where it represents the 
+ 	//data in our form
+ 	//An object w/3 properties: name, phone and size
+ 	$scope.party = {name:'', phone:'', size:''};//test this in the template(change to party var)
+
+ 	//create function to
+ 	$scope.saveParty = function() {
+ 		//Take the party object and put it into the parties array
+ 		//1st grab the parties array and push the party object into it
+ 		// $scope.parties.push($scope.party);
+
+ 		//Now, with firebase, we have to "add" instead of "push" data
+ 		//so we replace push with the $add method that's generated on line17
+ 		$scope.parties.$add($scope.party);
+ 		//Then reset party to show a blank form fields
+ 		$scope.party = {name:'', phone:'', size:''};
+ 	};
+
  }]);
- // These are a sample how to add extra ctrls
-  // controller('MyCtrl1', [function() {
-
-  // }])
-  // .controller('MyCtrl2', [function() {
-
-  // }]);
+ 
